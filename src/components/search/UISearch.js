1e7960e6f463a3b710eb0fda42be7d120640108c
Diana <divnvp@yandex.com>
2022-07-02 12:40:40 +0900

    update: search component

--- src/components/search/UISearch.js
+++ src/components/search/UISearch.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import makeSearch from "../../network/search.network";
+import "./style.scss";
+// Components
+import UIPage from "../page/UIPage";
+
 export const UISearch = () => {
+  const params = {
+    q: ""
+  };
+  let response = {};
+  const [ modelValue, setModelValue ] = useState("");
+
+  useEffect(() => {
+    if(modelValue.length) {
+      params.q = modelValue;
+      const timeoutId = setTimeout(makeSearchWithInput, 1 * 500);
+    
+       return () => clearTimeout(timeoutId);
+    }
+  }, [modelValue]);
+
+  const makeSearchWithInput = async() => {
+    try {
+      response = (await makeSearch(params)).json();
+    } catch(e) {
+      alert(e);
+    }
+  }
+
+  const handleChange = (event) => {
+    setModelValue(event.target.value);
+  }
+
   return (
-    <div>
-      <span>UISearch</span>
-    </div>
+    <UIPage>
+      <div className="searching">
+        <span>Искать во Вселенной</span>
+        <input
+          value={modelValue}
+          autoFocus
+          type="text"
+          placeholder="Введите..."
+          onChange={handleChange}
+          className="searching__input"
+        />
+      </div>
+    </UIPage>
   )
 }
