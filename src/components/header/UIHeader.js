770a4e2d93f438a312b7480f001dae253e3a1726
Diana <divnvp@yandex.com>
2022-07-02 12:41:19 +0900

    update: header component

--- src/components/header/UIHeader.js
+++ src/components/header/UIHeader.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import { HOME, SEARCH } from "../../router/routes-paths";
 import "./style.scss";
 
 export function UIHeader() {
   return (
     <div className="header">
-      <Link to="/" className="header-link">Домой</Link>
-      <Link to="/search" className="header-link">Поиск</Link>
+      <Link to={HOME} className="header-link">Домой</Link>
+      <Link to={SEARCH} className="header-link">Поиск</Link>
     </div>
   );
 }
