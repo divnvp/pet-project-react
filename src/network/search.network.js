4a6fd3cd49d3e06c7f256fbf4c09053e5600e2ad
Diana <divnvp@yandex.com>
2022-07-02 14:06:59 +0900

    update: make search method as async

--- src/network/search.network.js
+++ src/network/search.network.js
@@ -2,7 +2,9 @@ import { api } from "./api.network";
 
 export default function makeSearch(params) {
   const searchParams = new URLSearchParams(params);
-  return fetch(`${api.baseUrl}/search?${searchParams}` , {
+  
+  return fetch(
+    `${api.baseUrl}/search?${searchParams}` , {
     method: "GET"
-  });
+  }).then(r => r.json());
 }
