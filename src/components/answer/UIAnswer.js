88032bd4554b28586b143f000e4b66bf89c60048
Diana <divnvp@yandex.com>
2022-07-05 18:02:05 +0900

    add: answer of space

---
+++ src/components/answer/UIAnswer.js
@@ -0,0 +1,22 @@
+import UITableRow from "../table/UITableRow";
+
+export default function UIAnswer(props) {
+  console.log(props);
+  return (
+    <div>
+      <span>Вселенная ответила</span>
+
+      <table>
+        <tbody>
+          {/* {
+            props.collectionItems.map((item, index) =>
+              <tr>
+                <UITableRow key={index} value={item} />
+              </tr>
+            )
+          } */}
+        </tbody>
+      </table>
+    </div>
+  )
+}
